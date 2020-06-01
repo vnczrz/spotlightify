849e1e638cf72597d609f2f7e90b6a0945b6123f
vnczrz <53134067+vnczrz@users.noreply.github.com>
2020-05-31 20:28:07 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # remove _ from filename before use
 
 USERNAME = ""
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+CLIENT_ID = "87af50a5a32e4c949d0b3008caf86e44"
+CLIENT_SECRET = "ef38607baacf4a84b0d24d7108840d14"
